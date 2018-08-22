d9484e5d026b7b4ab4c325db22dd7ce9eedf8a27
Brad.Jolicoeur <brad@jolicoeurs.net>
2018-08-22 14:20:44 -0400

    Revert "Revert "initial commit""
    This reverts commit 4775093514cc426826c2ce1cb28c22f71295df85.

---
+++ pubsub/NSB.Example.Publisher/Program.cs
@@ -0,0 +1,56 @@
+﻿using NSB.Example.Contracs.Events;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSB.Example.Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            EndpointConfiguration endpointConfiguration = ConfigureNSB();
+
+            var endpointInstance = Endpoint.Start(endpointConfiguration)
+              .GetAwaiter().GetResult();
+
+            while (true)
+            {
+                endpointInstance.Publish<IExampleEvent>(
+                        messageConstructor: message =>
+                        {
+                            message.Message = "Hello world: " + Guid.NewGuid().ToString();
+                            //message.NewProperty = "Populated with: " + Guid.NewGuid().ToString();
+                        })
+                 .GetAwaiter().GetResult();
+
+                // Sleep as long as you need.
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static EndpointConfiguration ConfigureNSB()
+        {
+
+            var endpointConfiguration = new EndpointConfiguration("NSB.Example.Publisher");
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var conventions = endpointConfiguration.Conventions();
+            conventions.DefiningEventsAs(
+                type =>
+                {
+                    return type.Namespace != null &&
+                           type.Namespace.EndsWith("Events") ;
+                });
+
+            endpointConfiguration.EnableInstallers();
+
+            return endpointConfiguration;
+        }
+    }
+}
