f69a5f40638fb54ffb168a7830749b2680dbecec
Brad.Jolicoeur <brad@jolicoeurs.net>
2018-08-22 14:23:00 -0400

    recreate

---
+++ contracts/NSB.Example.Contracs/NSB.Example.Contracs/Events/IExampleEventV2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSB.Example.Contracs.Events
+{
+    public interface IExampleEventV2 : IExampleEvent
+    {
+        string NewProperty { get; set; }
+    }
+}
